e81a74ac1779f9875361444445e7bf2731039ffd
christian otkjær <christian.otkjaer@gmail.com>
2015-09-16 16:05:30 +0200

    Initial Commit

---
+++ Silverback/Silverback.h
@@ -0,0 +1,19 @@
+//
+//  Silverback.h
+//  Silverback
+//
+//  Created by Christian Otkjær on 16/09/15.
+//  Copyright © 2015 Christian Otkjær. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Silverback.
+FOUNDATION_EXPORT double SilverbackVersionNumber;
+
+//! Project version string for Silverback.
+FOUNDATION_EXPORT const unsigned char SilverbackVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Silverback/PublicHeader.h>
+
+
